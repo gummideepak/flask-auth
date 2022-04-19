eeeb779d372fb3be54284ccb7cb1ebf9ff993fcc
Deepak <gummideepak@gmail.com>
2022-04-19 17:01:42 -0500

    logging rework

--- app/simple_pages/__init__.py
+++ app/simple_pages/__init__.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
+import logging
 
 simple_pages = Blueprint('simple_pages', __name__,
                         template_folder='templates')
 
+log = logging.getLogger("myApp")
+
 
 @simple_pages.route('/')
 def index():
+    log.info()
     try:
         return render_template('index.html')
     except TemplateNotFound:
