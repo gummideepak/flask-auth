678df77f5580d45598669e0cfcd4352d60feb243
Deepak <gummideepak@gmail.com>
2022-04-29 19:19:57 -0500

    user login tests

--- tests/user_test.py
+++ tests/user_test.py
@@ -1,5 +1,6 @@
 import logging
-
+from flask import redirect
+from flask_login import login_user, login_required, logout_user, current_user
 from app import db
 from app.db.models import User, Song
 from faker import Faker
@@ -42,5 +43,60 @@ def test_adding_user(application):
         assert db.session.query(Song).count() == 0
 
 
+def test_user_regestration(client):
+    assert db.session.query(User).count() == 0
+    log = logging.getLogger("myApp")
+    token = str(client.get('/register').data)
+    start = token.find('name="csrf_token" type="hidden" value="')+len('name="csrf_token" type="hidden" value="')
+    token = token[start:]
+    end = token.find('"')
+    token = token[:end]
+    # log.info(token)
+    data = {
+        'email':'test@njit.edu',
+        'password': 'qwerty1234',
+        'confirm': 'qwerty1234',
+        'csrf_token': token
+    }
+    response = client.post('/register', data=data)
+    # log.info(response)
+    user = User.query.filter_by(email='test@njit.edu').first()
+    # log.info(user)
+    assert user.email == 'test@njit.edu'
+    assert db.session.query(User).count() == 1
+    return user
 
+def test_user_login(application, client):
+    with application.app_context():
+        token = str(client.get('/login').data)
+        token = extract_csrf_token(token)
+        # log.info(token)
+        data = {
+            'email':'test@njit.edu',
+            'password': 'qwerty1234',
+            'csrf_token': token
+        }
+        response = client.post('/login', data=data, follow_redirects=True)
+        assert b'Welcome' in response.data
+
+
+def test_user_login_access(application, client):
+    with application.app_context():
+        token = str(client.get('/login').data)
+        token = extract_csrf_token(token)
+        data = {
+            'email':'test@njit.edu',
+            'password': 'qwerty1234',
+            'csrf_token': token
+        }
+        response = client.post('/login', data=data, follow_redirects=True)
+        response = client.get('/users', follow_redirects=True)
+        assert response.status_code == 200
+
+def extract_csrf_token(page):
+    start = page.find('name="csrf_token" type="hidden" value="')+len('name="csrf_token" type="hidden" value="')
+    page = page[start:]
+    end = page.find('"')
+    page = page[:end]
+    return page
 
